f534752be722d4a186d0a5beb924269078fc0cc1
stekra <stekra@me.com>
2024-01-28 14:00:45 +0100

    some build settings

--- Assets/Scripts/GameControllerBehaviour.cs
+++ Assets/Scripts/GameControllerBehaviour.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.UI;
 
