04332a97a960ad1a4fcdc43aa2c7880cacd4a425
stekra <stekra@me.com>
2024-01-28 10:39:23 +0100

    sphere target warning blink fx

--- Assets/Scripts/SphereTarget.cs
+++ Assets/Scripts/SphereTarget.cs
@@ -7,13 +7,20 @@ public class SphereTarget : MonoBehaviour {
     private float minInterval;
     [SerializeField]
     private float maxInterval;
+    private float remainingTime;
     private List<SphereComponent> spheres = new List<SphereComponent>();
 
     private GameControllerBehaviour gameController;
 
     public GameObject submitVFXObject;
+    public GameObject warnVFXObject;
+    // First blinks slow, then fast. Total time is slowBlinkTime + fastBlinkTime in seconds.
+    public float slowBlinkTime = 3f;
+    public float fastBlinkTime = 1f;
+    private Coroutine blink;
 
     void OnTriggerEnter(Collider other) {
+        if (other == null) return;
         SphereComponent sphere = other.GetComponent<SphereComponent>();
         if (sphere != null) {
             spheres.Add(sphere);
@@ -21,11 +28,11 @@ public class SphereTarget : MonoBehaviour {
     }
 
     void OnTriggerExit(Collider other) {
+        if (other == null) return;
         SphereComponent sphere = other.GetComponent<SphereComponent>();
         if (spheres.Contains(sphere))
             spheres.Remove(sphere);
     }
-
 
     void Start() {
         gameController = FindObjectOfType<GameControllerBehaviour>();
@@ -34,22 +41,39 @@ public class SphereTarget : MonoBehaviour {
 
     private IEnumerator DeliverTimer() {
         float duration = Random.Range(minInterval, maxInterval);
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(duration - slowBlinkTime - fastBlinkTime);
+
+        blink = StartCoroutine(Blink(0.3f));
+        yield return new WaitForSeconds(slowBlinkTime);
+        StopCoroutine(blink);
+        blink = StartCoroutine(Blink(0.15f));
+        yield return new WaitForSeconds(fastBlinkTime);
+
         StartCoroutine(Deliver());
         StartCoroutine(DeliverTimer());
     }
 
     private IEnumerator Deliver() {
         submitVFXObject.GetComponent<Animator>().SetTrigger("Submit");
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.7f);
         foreach (SphereComponent sphere in spheres){
             gameController.RateSphere(sphere);
             Destroy(sphere.gameObject);
         }
         spheres.Clear();
+
+        StopCoroutine(blink);
+        warnVFXObject.SetActive(false);
     }
 
     void OnDisable() {
         StopAllCoroutines();
+    }
+
+    private IEnumerator Blink(float interval) {
+        while (true) {
+            warnVFXObject.SetActive(!warnVFXObject.activeSelf);
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
