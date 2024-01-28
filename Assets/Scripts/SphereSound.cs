1fdb581bf5ef3571a4a41d6d1dc66ea52232a841
ringdinglinn <lalaponii@gmail.com>
2024-01-28 10:47:07 +0100

    bop added

---
+++ Assets/Scripts/SphereSound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSound : MonoBehaviour {
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    MeshRenderer mrenderer;
+    float initPitch;
+
+    void Start() {
+        initPitch = audioSource.pitch;
+    }
+
+    void OnCollisionEnter(Collision collision){
+        Debug.Log("play sound");
+        if (collision.relativeVelocity.magnitude > 3 && mrenderer.isVisible) {
+            audioSource.pitch = Random.Range(initPitch - 0.1f, initPitch + 0.1f);
+            audioSource.Play();
+        }
+    }
+}
